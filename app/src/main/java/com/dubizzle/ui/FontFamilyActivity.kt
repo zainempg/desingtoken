d04130fa4583bd206b380fa7d77a2d60e1aa66b6
findxain <100686691+zainempg@users.noreply.github.com>
2024-06-03 15:15:18 +0500

    Typography Paragraph and label

--- app/src/main/java/com/dubizzle/ui/FontFamilyActivity.kt
+++ app/src/main/java/com/dubizzle/ui/FontFamilyActivity.kt
@@ -1,14 +1,23 @@
 package com.dubizzle.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
+
 
 class FontFamilyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setLocale()
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_font_family)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +25,41 @@ class FontFamilyActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.buttonChangeLanguage).apply {
+            text = Locale.getDefault().language
+        }.setOnClickListener {
+            val pref = getSharedPreferences("localePref", MODE_PRIVATE)
+
+            pref.apply {
+                val isAr = getBoolean("isAr", false)
+                edit().putBoolean("isAr", isAr.not()).apply()
+            }
+            setLocale()
+        }
+
+    }
+
+    private fun setLocale() {
+        val pref = getSharedPreferences("localePref", MODE_PRIVATE)
+        val isAr = pref.getBoolean("isAr", false)
+        val locale = Locale(if (isAr) "ar" else "en")
+        Locale.setDefault(locale)
+
+        val config: Configuration = baseContext.resources.configuration
+        config.locale = locale
+        baseContext.resources.updateConfiguration(
+            config,
+            baseContext.resources.displayMetrics
+        )
+        val localeListCompat = LocaleListCompat.forLanguageTags(locale.toLanguageTag())
+        AppCompatDelegate.setApplicationLocales(localeListCompat)
+
+    }
+
+    private fun restartActivity() {
+        val intent = intent
+        finish()
+        startActivity(intent)
     }
 }
