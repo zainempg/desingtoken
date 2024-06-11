cc2f49124bac912d359bd3186a1fcba49258aa6a
zainempg <zainul.abideen@dubizzlelabs.com>
2024-06-11 14:23:28 +0500

    ssp sdp added

--- dubizzleui/build.gradle.kts
+++ dubizzleui/build.gradle.kts
@@ -44,4 +44,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    api (libs.ssp.android)
+    api (libs.sdp.android)
+
+
 }
